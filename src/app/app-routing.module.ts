5f0837d5ea9fd7e5a4ebd1179126419098efbfda
ghofranetayari <ghofrane.tayari@esprit.tn>
2024-02-27 10:33:18 +0100

    commit1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllTemplateBackComponent } from './BackOffice/all-template-back/all-template-back.component';
+import { AllTemplateFrontComponent } from './FrontOffice/all-template-front/all-template-front.component';
+import { HomeFrontComponent } from './FrontOffice/home-front/home-front.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"" , 
+    component:AllTemplateFrontComponent,
+    children:[
+      {
+        path:"",
+        component:HomeFrontComponent,
+        
+      }
+    ]
+  },
+
+  {
+    path:"admin" , 
+    component:AllTemplateBackComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllTemplateBackComponent } from './BackOffice/all-template-back/all-template-back.component';
+import { FooterBackComponent } from './BackOffice/footer-back/footer-back.component';
+import { NavbarBackComponent } from './BackOffice/navbar-back/navbar-back.component';
+import { SidebarBackComponent } from './BackOffice/sidebar-back/sidebar-back.component';
+import { AllTemplateFrontComponent } from './FrontOffice/all-template-front/all-template-front.component';
+//import { SidebarFrontComponent } from './FrontOffice/sidebar-front/sidebar-front.component';
+import { FooterFrontComponent } from './FrontOffice/footer-front/footer-front.component';
+import { HeaderFrontComponent } from './FrontOffice/header-front/header-front.component';
+import { HomeFrontComponent } from './FrontOffice/home-front/home-front.component';
+import { HomeBackComponent } from './BackOffice/home-back/home-back.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllTemplateBackComponent,
+    FooterBackComponent,
+    NavbarBackComponent,
+    SidebarBackComponent,
+    AllTemplateFrontComponent,
+  //  SidebarFrontComponent,
+    FooterFrontComponent,
+    HeaderFrontComponent,
+    HomeFrontComponent,
+    HomeBackComponent
   ],
   imports: [
     BrowserModule,
